b87b14a01654a47f3f70bdbe85dd9e5269064a0c
Lou Woell <lou.woell@posteo.de>
2024-12-06 00:04:56 +0100

    config via toml file

--- cleandir.scm
+++ cleandir.scm
@@ -1,71 +1,166 @@
-(import
- (srfi-1)
- (chicken file)
- (chicken file posix)
- (chicken pathname)
- (pathname-expand)
- (chicken keyword)
- (chicken time)
- (chicken time posix))
+(module readconfig
+    (read-config-file)
 
-(define lou-rules
-  '(#:basedir "~/Downloads/"
-              #:groups (("pdf" . ((#:dir . "PDF")))
-                        ("odt" . ((#:dir . "office")))
-                        ("docx" . ((#:dir . "office")))
-                        (#f . ((#:dir . ""))))))
+  (import
+    (scheme)
+    (only srfi-1 append-map)
+    (toml)
+    (chicken base)
+    (chicken file))
 
-(define (classify-file x)
-  (if (not (directory? x))
-      (cons (pathname-extension x) x)
-      (cons "dir" x)))
+  (define toml-keys '(("enabled" . #:bool)
+                      ("directory" . #:string)
+                      ("target" . #:string)
+                      ("extension" . #:string)
+                      ("extensions" . #:list)))
+  
+  (define (toml-array->list table key)
+    (let* ((array (toml-array table key))
+           (values (toml-count-entries array)))
+      (read-array-values array 0 values)))
 
-(define (classify-files dir)
-   (map (lambda (x) (classify-file
-                (make-absolute-pathname dir x)))
-        (directory (normalize-pathname dir))))
+  (define toml-getters `((#:string . ,toml-string)
+                         (#:bool . ,toml-bool)
+                         (#:list . ,toml-array->list)))
 
-(define (lou-ft? ft)
-  (lambda (x) (equal? (car x)
-                 ft)))
-                     
-(define (group-files-fn ext)
-  (lambda (x) (cons ext (map cdr (filter (lou-ft? ext) x)))))
+  (define (get-key-value table key)
+    (when (toml-key-exists? table key)
+      (and-let* ((type (assoc key toml-keys))
+                 (getter (cdr (assoc (cdr type) toml-getters))))
+        (getter table key))))
 
-(define (group-files exts files)
-  (map (lambda (x) (x files))
-       (map group-files-fn exts)))
+  (define (list-tables table)
+    (let ((tab-info (table-info table)))
+      (get-tables table (car tab-info) (+ (car tab-info)
+                                          (cdr tab-info)))))
 
-(define (group-directory-files dir rules)
-    (map (lambda (x) (cons  (assoc (car x) rules)
+  (define (get-tables table index max)
+    (if (= index max)
+        '()
+        (let ((key (toml-key-at table index)))
+          (cons key (get-tables table (add1 index) max)))))
+
+  (define (read-config-file file)
+    (let ((table (table-from-file file)))
+      (map (lambda (x) (directory-rule (toml-table table x)))
+           (list-tables table))))
+
+  (define (read-array-values array index max)
+    (if (= index max)
+        '()
+        (cons (toml-string array index)
+              (read-array-values array (add1 index) max))))
+  
+  (define (table-info table)
+    (cons  (+ (toml-count-key-vals table)
+              (toml-count-arrays table))
+           (toml-count-tables table)))
+
+  (define (directory-rule table)
+    (when (get-key-value table "enabled")
+      (list (cons #:basedir (get-key-value table "directory"))
+            (cons #:targetbase "test")
+            (cons #:groups (append
+                            (append-map (lambda (x) (group-rule (toml-table table x)))
+                                        (list-tables table))
+                            (list (cons #f (list (cons #:dir "")))))))))
+
+  (define (extension-rule extension target)
+    (cons extension (list (cons #:dir target))))
+
+  (define (group-rule table)
+    (when (get-key-value table "enabled")
+      (cond ((toml-key-exists? table "extension")
+             (list (extension-rule (get-key-value table "extension")
+                                   (get-key-value table "target"))))
+            ((toml-key-exists? table "extensions")
+             (map (lambda (x) (extension-rule x (get-key-value table "target")))
+                  (get-key-value table "extensions"))))))
+  )
+
+(module cleandir
+    (clean-dir)
+
+  (import
+    (readconfig)
+    (scheme)
+    (only srfi-1 filter)
+    (chicken base)
+    (chicken file)
+    (chicken file posix)
+    (chicken pathname)
+    (pathname-expand)
+    (chicken time posix))
+
+  (define (classify-file x groups)
+    (let ((ext (pathname-extension x)))
+      (cond  ((directory? x) (cons "dir" x))
+             ((assoc ext groups) (cons ext x))
+             (else (cons #f x)))))
+
+  (define (classify-files dir groups)
+    (map (lambda (x) (classify-file (make-absolute-pathname dir x)
+                               groups))
+         (directory (normalize-pathname dir))))
+
+  (define (ft? ft)
+    (lambda (x) (equal? (car x) ft)))
+  
+  (define (group-files-fn ext)
+    (lambda (x) (cons ext (map cdr (filter (ft? ext) x)))))
+
+  (define (group-files exts files)
+    (map (lambda (x) (x files))
+         (map group-files-fn exts)))
+
+  (define (group-directory-files dir groups)
+    (map (lambda (x) (cons  (assoc (car x) groups)
                        (cdr x)))
-         (group-files (map (lambda (x) (car x)) rules)
-                      (classify-files dir))))
+         (group-files (map (lambda (x) (car x)) groups)
+                      (classify-files dir groups))))
 
-(define (year-month)
-  (time->string (seconds->local-time) "%Y-%m"))
+  (define (year-month)
+    (time->string (seconds->local-time) "%Y-%m"))
 
-(define (move-files target-dir files)
-  (map (lambda (x)
-         (move-file x (pathname-replace-directory x target-dir)))
-       files))
+  (define (move-files target-dir files)
+    (map (lambda (x)
+           (let ((target (pathname-replace-directory x target-dir)))
+             (if (file-exists? target)
+                 (print target " exists ... skipping")
+                 (begin (print x " -> " target)
+                        (move-file x target)))))
+         files))
 
-(define (apply-rule group target-dir)
-  (let* ((rule (cdar group))
-         (files (cdr group))
-         (target-dir (make-absolute-pathname target-dir
-                                             (alist-ref #:dir rule))))
-    (create-directory target-dir #t)
-    (move-files target-dir files)))
+
+  (define (apply-rule target-dir)
+    (lambda (group)
+      (let* ((rule (cdar group))
+             (files (cdr group))
+             (target-dir (make-absolute-pathname target-dir
+                                                 (alist-ref #:dir rule))))
+        (create-directory target-dir #t)
+        (move-files target-dir files))))
   
 
-(define (clean-dir)
-  (let* ((basedir (pathname-expand (get-keyword #:basedir lou-rules)))
-        (rules (get-keyword #:groups lou-rules))
-        (target-dir (make-absolute-pathname basedir (year-month)))
-        (groups (group-directory-files basedir rules)))
-    (map print groups)
-    (map (lambda (x) (apply-rule x target-dir))
-         groups)))
+  (define (clean-dir rules)
+    (let* ((basedir (pathname-expand (alist-ref #:basedir rules)))
+           (groupdefs (alist-ref #:groups rules))
+           (target-dir (make-absolute-pathname basedir (year-month)))
+           (groups (group-directory-files basedir groupdefs)))
+      (print basedir " -> " target-dir)
+      (map print groups)
+      (map (apply-rule target-dir) groups)
+      ))
+  )
 
-(clean-dir)
+(module main
+    ()
+    
+  (import
+    (scheme)
+    (readconfig)
+    (pathname-expand)
+    (cleandir))
+  
+  (define default-config-file (pathname-expand "~/.config/cleandir/config.toml"))
+  (map clean-dir (read-config-file default-config-file)))
--- makefile
+++ makefile
@@ -3,11 +3,14 @@ BIN= clean-dload-dir
 
 PREFIX= $(HOME)/.local/
 
-$(BIN): 
-	$(CC) cleandir.scm -o $@
+$(BIN): cleandir.scm
+	$(CC) $^ -o $@ -vv
 
 .Phony: install
 install:
-	install $(BIN) "$(PREFIX)/bin"
+	install -m u+wx,a+r $(BIN) "$(PREFIX)/bin"
+	install -m u+w,a+r cleandloads.timer   $(HOME)/.config/systemd/user/
+	install -m u+w,a+r cleandloads.service $(HOME)/.config/systemd/user/
+	install -m u+w,a+r -D config.toml -t $(HOME)/.config/cleandir/
 uninstall:
 	rm "$(PREFIX)/bin/$(BIN)"
